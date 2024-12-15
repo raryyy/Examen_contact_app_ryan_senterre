e4711ca838c9748636e7b3c56543aa2e86acdde4
RARY <ryan.senterre@gmail.com>
2024-12-15 14:19:46 +0100

    Display computed contact count

--- src/components/ContactsListMain.vue
+++ src/components/ContactsListMain.vue
@@ -1,27 +1,44 @@
 <script setup>
 import ContactsList from "@/components/ContactsList.vue";
+import NewContactInput from "@/components/NewContactInput.vue";
+import { ref } from "vue";
 import { useContactsStore } from "@/stores/contacts";
 
 const contactsStore = useContactsStore();
+const contactToEdit = ref(null);
+
+const startEditing = (contact) => {
+  contactToEdit.value = { ...contact };
+};
+
+const stopEditing = () => {
+  contactToEdit.value = null;
+};
 </script>
 
 <template>
   <main class="container mx-auto px-4 mt-6 flex-grow">
-  <div class="flex flex-col lg:flex-row gap-6">
-  <section class="w-full lg:w-2/3 bg-white rounded-lg shadow p-6">
-    <h2 class="text-xl mb-4 flex justify-between">
-      <span class="font-semibold">Contacts</span>
-      <span class="text-sm">x item(s)</span>
-    </h2>
-    <div class="flex items-center mb-4">
-            <input
-              type="text"
-              placeholder="Search contacts..."
-              class="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-    <ContactsList :contacts="contactsStore.contacts" />
-  </section>
-  </div>
+    <div class="flex flex-col lg:flex-row gap-6">
+      <section class="w-full lg:w-2/3 bg-white rounded-lg shadow p-6">
+        <h2 class="text-xl mb-4 flex justify-between">
+          <span class="font-semibold">Contacts</span>
+          <span class="text-sm">{{ contactsStore.contacts.length }} item(s)</span>
+        </h2>
+        <div class="flex items-center mb-4">
+          <input
+            type="text"
+            placeholder="Search contacts..."
+            class="w-full p-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <ContactsList
+          :contacts="contactsStore.contacts"
+          @edit-contact="startEditing"
+        />
+      </section>
+      <section v-if="contactToEdit" class="w-full lg:w-1/3">
+        <NewContactInput :contact="contactToEdit" @save="stopEditing" />
+      </section>
+    </div>
   </main>
 </template>
